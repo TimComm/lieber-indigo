42c131b680d742d3a021e7eaa6813b6f554f827f
Thinkpad <thinkpad@example.com>
2025-07-10 21:13:56 +0200

    Remove deprecated packages that cause trouble.

--- src/app/secret/page.tsx
+++ src/app/secret/page.tsx
@@ -1,12 +1,10 @@
 
 'use client';
 
-import Tesseract from '@/components/Tesseract';
-
 export default function SecretPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
-      <Tesseract />
+    <span>Tesseract + p5 is under construction :/</span>
     </div>
   );
 }
